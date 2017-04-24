9e1f53fdc5d7168a58262e9459889883007a2eab
Azat Mardan <hi@azat.co>
2017-04-24 13:26:07 -0700

    add changes :high_heel:

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,4 +7,3 @@ curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y nodejs
 # install pm2 to restart node app
 npm i -g pm2@2.4.3
-sudo chmod 755 /var/www/slow-hello-world.js # optional
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sudo chmod 755 /var/www/slow-hello-world.js # optional
 # this will restart app/server on instance reboot
 crontab -l | { cat; echo "@reboot pm2 start /var/www/slow-hello-world.js -i 0 --name \"node-app\""; } | crontab -
 # actually start the server
