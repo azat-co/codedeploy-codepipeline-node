bb2132eaf1b6b90d319a7441ac6a6fa481ac6351
Azat Mardan <hi@azat.co>
2017-04-24 13:46:59 -0700

    add changes :horse_racing:

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-isExistApp=`pgrep pm2`
+isExistApp=`sudo pgrep pm2`
 if [[ -n  $isExistApp ]]; then
     # sudo pm2 kill
     sudo pm2 stop node-app
+    # you can remove the crontab as well
+fi
+
+isExistApp=`pgrep pm2`
+if [[ -n  $isExistApp ]]; then
+    # sudo pm2 kill
+    echo 'yo'
     # you can remove the crontab as well
 fi
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('http')
   .createServer((req, res) => {
     console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
     res.writeHeader(200,{'Content-Type': 'text/html'})
-    res.end('<h1>hello world from codedeploy and codepipeline</h1>')
+    res.end('<h1>Hello World from CodeDeploy and CodePipeline</h1>')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
