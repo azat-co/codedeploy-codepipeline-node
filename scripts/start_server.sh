e5cdc2f956e9c538f1cd681fe6c389538b3354c8
Azat Mardan <hi@azat.co>
2017-04-24 11:49:53 -0700

    add changes :fishing_pole_and_fish:

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 # this will restart app/server on instance reboot
 crontab -l | { cat; echo "@reboot pm2 start /var/www/slow-hello-world.js -i 0 --name \"node-app\""; } | crontab -
 # actually start the server
