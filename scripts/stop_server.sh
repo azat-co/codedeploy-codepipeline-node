39d9b268a1122acfbc76e25f68bb6a8dc666440d
Azat Mardan <hi@azat.co>
2017-04-25 10:45:39 -0700

    add changes :astonished:

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-isExistApp=`sudo pgrep pm2`
-if [[ -n  $isExistApp ]]; then
-    # sudo pm2 kill
-    sudo pm2 stop node-app
-    # you can remove the crontab as well
-fi
+sudo pm2 stop node-app
 
-isExistApp=`pgrep pm2`
-if [[ -n  $isExistApp ]]; then
+# isExistApp=`sudo pgrep pm2`
+# if [[ -n  $isExistApp ]]; then
     # sudo pm2 kill
-    echo 'yo'
+    # sudo pm2 stop node-app
     # you can remove the crontab as well
-fi
+# fi
+
+# isExistApp=`pgrep pm2`
+# if [[ -n  $isExistApp ]]; then
+#     # sudo pm2 kill
+#     echo 'yo'
+#     # you can remove the crontab as well
+# fi
