8c961c51fcf4c77eb46086dc70f13ce5b9a54dda
Azat Mardan <azat-co@users.noreply.github.com>
2017-07-01 13:24:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('http')
   .createServer((req, res) => {
     console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
     res.writeHeader(200,{'Content-Type': 'text/html'})
-    res.end('<h1>Hello World from CodeDeploy and CodePipeline</h1>')
+    res.end('<h1>Hello World from amazing Node University course AWS Intermediate</h1>')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
