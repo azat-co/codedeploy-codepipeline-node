cc55d451b76e591f89c13960983d5f34f561d286
Azat Mardan <azat-co@users.noreply.github.com>
2019-03-01 11:38:21 -0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('http')
   .createServer((req, res) => {
     console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
     res.writeHeader(200,{'Content-Type': 'text/html'})
+    res.write('<p>Buenos dias Montevideo</p>')
     res.end('<h1>Hello World from amazing Node University course AWS Intermediate</h1>')
   })
   .listen(port, (error)=>{
